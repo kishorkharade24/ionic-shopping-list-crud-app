89c9ae5494f61a9d1d703102722307237f7062db
Kishor Kharade <kishor.kharade@atmecs.com>
2017-11-23 11:50:04 +0530

    Added firebase config

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyD_pn4rcbkonjMhgXskE5y3Qq1FAxIKAZ4",
+  authDomain: "ionic-shopping-crud-app.firebaseapp.com",
+  databaseURL: "https://ionic-shopping-crud-app.firebaseio.com",
+  projectId: "ionic-shopping-crud-app",
+  storageBucket: "",
+  messagingSenderId: "82289261832"
+};
